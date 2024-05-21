57246e2de7f22b48532a0bee7ba8db1570f75c6c
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-05-21 21:26:50 +0200

    constuctor initializes the store with a reducer and an initial state

--- scripts.js
+++ scripts.js
@@ -1,20 +1,14 @@
-{ type: 'INCREMENT'}
-{ type: 'DECREMENT'}
+// Scripts.js
 
-
-function counter(state = 0, action) {
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1;
-            case 'DECREMENT':
-                 return state -1;
-                 default:
-                    return state;
+// Store class to manage state
+class Store {
+    // Constructor initializes the store with a reducer and an initial state
+    constructor(reducer, initialState) {
+        this.reducer = reducer;
+        this.state = initialState;
+        this.listeners = []; // List of subscribed listeners
     }
+
+    
 }
-
-let store = Redux.createStore(counter);
-
-store.subscribe(() => console.log(store.getState()));
-
 
