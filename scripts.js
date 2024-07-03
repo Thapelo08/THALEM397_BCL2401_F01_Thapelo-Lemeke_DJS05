2c973bf647ddbd6ba34a78d7bc9eb9fe6cc75e2d
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-03 15:51:23 +0200

    "Added getState and dispatch methods to createStore function"

--- scripts.js
+++ scripts.js
@@ -5,6 +5,14 @@ import { ADD, SUBSTRACT, RESET } from "./actions"; // Imports action types
 function createStore(reducer) {
   let currentState = reducer(undefined, {});
   let listeners = [];
-  
+  // Function to get current state and to dispatch actions then update state
+  return {
+    getState: () => currentState,
+    dispatch: (action) => {
+      currentState = reducer(currentState, action);// Function that update the state
+      listeners.forEach((listener) => listener()); // Function that notifies all listeners
+    },
+  }
+
 
 }
