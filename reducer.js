cefbdcd99acdb0dc393d1d6e26abc1795338f631
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-03 15:19:26 +0200

    "Added reducer function skeleton with imports and switch statement"

--- reducer.js
+++ reducer.js
@@ -1,2 +1,9 @@
 import { initialState } from "./initialState"; // Imports initial state for the counter
 import { ADD, SUBTRACT, RESET } from "./actions"; // Imports action types
+
+// Reducer function to manage state updates for the counter
+export function reducer(state = initialState,action) {
+    swicth (action.type) {
+        
+    }
+}
