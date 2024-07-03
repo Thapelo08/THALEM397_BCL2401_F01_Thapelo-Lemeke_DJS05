0108375e751abf951e6817ab35a58b85a0e60a5e
Thapelo Lemeke <thapelolemeke15@gmail.com>
2024-07-03 15:22:44 +0200

    "Added case for ADD action in reducer function"

--- reducer.js
+++ reducer.js
@@ -4,6 +4,10 @@ import { ADD, SUBTRACT, RESET } from "./actions"; // Imports action types
 // Reducer function to manage state updates for the counter
 export function reducer(state = initialState,action) {
     swicth (action.type) {
-        
+        case ADD:
+            // Increment count by 1 when ADD action is dispateched
+            return { count: state.count + 1 };
+            
+
     }
 }
